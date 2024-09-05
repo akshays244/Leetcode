b5038a28a932bf44828d146e8e6f4d212edc89aa
akshays244 <akshays244@gmail.com>
2024-09-05 11:27:57 +0530

    pycharm check 1

---
+++ longes common prefix.py
@@ -0,0 +1,12 @@
+def longestCommonPrefix(strs):
+    a=[]
+    b=[]
+    for st in range((len(strs))):
+        print(st)
+
+
+
+
+    return a
+
+print(longestCommonPrefix(['flower','flying','fleet']))
