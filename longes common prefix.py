55999098a7b39042e38f801ba5125f5aa33c9e12
akshays244 <akshays244@gmail.com>
2024-09-05 15:50:30 +0530

    pycharm check 1

--- longes common prefix.py
+++ longes common prefix.py
@@ -1,12 +1,22 @@
+from sys import prefix
+
+
 def longestCommonPrefix(strs):
     a=[]
     b=[]
-    for st in range((len(strs))):
-        print(st)
+    prefix = strs[0]
+
+    for strings in strs[1:]:
+        i = 0
+        while i < len(prefix) and i <len(strings) and prefix[i] == strings[i]:
+            i += 1
+        prefix= prefix[:i]
+
+        if not prefix:
+            return ""
+
+    return prefix
 
 
-
-
-    return a
 
 print(longestCommonPrefix(['flower','flying','fleet']))
